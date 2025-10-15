0b077436906b316f6d5e35d5bd9eca19f8e96877
Daria bc2025-3 <kozikovadasa@gmail.com>
2025-10-15 11:48:35 +0300

    changed the main file, installed a new package

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-const {program} = require('commander');
+const { program } = require('commander');
 const fs = require('fs');
+const http = require('http');
 
+program
+  .option('-i, --input <path>', 'path to the file we are giving for reading')
+  .option('-h, --host <host>', 'server address')
+  .option('-p, --port <port>', 'server port');
+
+program.parse();
+
+const options = program.opts(); 
+
+if (!fs.existsSync(options.input)) {
+  console.error('Cannot find input file');
+  process.exit(1);
+}
+
+if (!options.input || !options.host || !options.port) {
+  console.error('Please provide all required parameters: --input, --host, --port');
+  process.exit(1);
+}
+
+const server = http.createServer((req, res) => {
+  const data = fs.readFileSync(options.input, 'utf8');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`📄 File contents:\n\n${data}`);
+});
+
+server.listen(options.port, options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}`);
+});
